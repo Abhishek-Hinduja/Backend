e6e472e1ce3f0949afcb5defb35d16534dc0bd5a
Abhishek12589 <hindujaabhishek5@gmail.com>
2024-02-14 15:06:44 +0530

    Working With FileSystem Library

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const http = require('http');
-
+const fs = require('fs')
 var port = 4000
 
 
 const server = http.createServer((req,res)=>{
     if (req.method == "GET"){
         if (req.url == "/"){
-            res.end("Welcome to the home page")
+            fs.readFile("./index.html", "utf-8", (err,data)=>{
+                res.end(data)
+            })
         }
         else if(req.url == "/about"){
             res.end("Welcome to the About page")
@@ -23,3 +25,6 @@ const server = http.createServer((req,res)=>{
 server.listen(port,()=>{
     console.log(`Server is running on ${port}`)
 })
+
+
+
